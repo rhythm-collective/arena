2df4bded0acd840784260e063e763fc941d1b71f
Zageron <hello@zageron.com>
2020-01-10 23:03:25 -0800

    Add sample local nng test.

--- arena/core/sample.py
+++ arena/core/sample.py
@@ -26,4 +26,12 @@ def hello_mongo(uri: str) -> None:
     print(scratch_db.last_status())
 
 def hello_nng():
-    pass
+    from pynng import Pair0
+    peer_one = Pair0()
+    peer_one.listen('tcp://127.0.0.1:54321')
+    peer_two = Pair0()
+    peer_two.dial('tcp://127.0.0.1:54321')
+    peer_one.send(b'Well hello there')
+    print(peer_two.recv())
+    peer_one.close()
+    peer_two.close()
